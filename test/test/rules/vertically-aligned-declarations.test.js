e61ed12dda638af609eea707d45285eaaa68c44d
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-01-24 14:45:03 +0100

    add declaration unalignment test

--- test/test/rules/vertically-aligned-declarations.test.js
+++ test/test/rules/vertically-aligned-declarations.test.js
@@ -3,6 +3,17 @@ const { contractWith } = require('solhint/test/common/contract-builder');
 const { assertErrorCount, assertErrorMessage, assertNoErrors } = require('solhint/test/common/asserts');
 
 describe('Linter - vertically aligned comments', () => {
+    it('should report vertically unaligned comments', () => {
+        const processedStr = contractWith(require('../fixtures/declarations/unalignedDeclarations'));
+        const report = linter.processStr(processedStr, {
+            plugins: ['makerdao'],
+            rules: {
+                'makerdao/vertically-aligned-declarations': 'error',
+            },
+        });
+        assertErrorCount(report, 1);
+        assertErrorMessage(report, 'State variable declarations should be aligned');
+    });
     it('should not report vertically aligned comments', () => {
         const processedStr = contractWith(require('../fixtures/declarations/alignedDeclarations'));
         const report = linter.processStr(processedStr, {
