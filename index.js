8f8800097ce7839d9238e3edd79360fdbd3212b3
KirillDogadin-std <kirillsc2@gmail.com>
2023-07-13 15:32:27 +0200

    dont out tess

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const { VerticallyAlignedComments } = require('./out/src/rules/vertically-aligned-comments');
-const { VerticallyAlignedDeclarations } = require('./out/src/rules/vertically-aligned-declarations');
-const { VerticallyAlignedVisibilityModifiers } = require('./out/src/rules/vertically-aligned-state-var-visibility');
-const { VerticallyAlignedAssignments } = require('./out/src/rules/vertically-aligned-assignments');
-const { NoNewlinesBetweenFunctionSignatures } = require('./out/src/rules/no-newlines-between-function-signatures');
-const { UnderscoredConstructorArguments } = require('./out/src/rules/constructor-arguments-unserscored');
-const { PreferTypeProvidedMax } = require('./out/src/rules/prefer-type-provided-max');
-const { CapitalizedSnakeOnlyForConst } = require('./out/src/rules/capitalized-snake-only-for-const');
+const { VerticallyAlignedComments } = require('./out/rules/vertically-aligned-comments');
+const { VerticallyAlignedDeclarations } = require('./out/rules/vertically-aligned-declarations');
+const { VerticallyAlignedVisibilityModifiers } = require('./out/rules/vertically-aligned-state-var-visibility');
+const { VerticallyAlignedAssignments } = require('./out/rules/vertically-aligned-assignments');
+const { NoNewlinesBetweenFunctionSignatures } = require('./out/rules/no-newlines-between-function-signatures');
+const { UnderscoredConstructorArguments } = require('./out/rules/constructor-arguments-unserscored');
+const { PreferTypeProvidedMax } = require('./out/rules/prefer-type-provided-max');
+const { CapitalizedSnakeOnlyForConst } = require('./out/rules/capitalized-snake-only-for-const');
 const {
     NewlinesBetweenCustomAndNativeDeclarations,
-} = require('./out/src/rules/newlines-between-custom-and-native-declarations');
+} = require('./out/rules/newlines-between-custom-and-native-declarations');
 
 module.exports = [
     VerticallyAlignedComments,
--- out/test/fixtures/functionSignatureNewLines/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const correct_1 = require("./correct");
-const incorrect_1 = require("./incorrect");
-exports.default = {
-    correct: correct_1.default,
-    incorrect: incorrect_1.default,
-};
