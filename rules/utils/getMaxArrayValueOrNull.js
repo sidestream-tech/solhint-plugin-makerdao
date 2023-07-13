02cf228307e9658533a023799e6a8344bff3dd35
KirillDogadin-std <kirillsc2@gmail.com>
2023-07-13 10:09:35 +0200

    fix tsconfig

--- rules/utils/getMaxArrayValueOrNull.js
+++
@@ -1,8 +0,0 @@
-function getMaxArrayValueOrNull(array) {
-    if (array.every(item => item === null)) {
-        return item;
-    }
-    return Math.max(...array);
-}
-
-module.exports = getMaxArrayValueOrNull;
