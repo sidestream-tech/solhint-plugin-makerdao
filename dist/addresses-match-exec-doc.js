2d5fed44427559b902da886c9272664dd4e17b80
KirillDogadin-std <kirillsc2@gmail.com>
2023-07-24 10:34:13 +0200

    tsc

--- dist/addresses-match-exec-doc.js
+++ dist/addresses-match-exec-doc.js
@@ -9,32 +9,46 @@ const os_1 = require("os");
 const path_1 = require("path");
 const parser_1 = require("@solidity-parser/parser");
 const sync_fetch_1 = __importDefault(require("sync-fetch"));
-// TODO
 const goodCode = `
-AUF
+pragma solidity 0.4.4;
+
+
+// if the executive document contains an address 0x0000000000000000000000000000000000000000
+contract C {
+    // Hash: cast keccak -- "$(wget 'https://raw.githubusercontent.com/makerdao/community/e4bf988dd35f82e2828e1ce02c6762ddd398ff92/governance/votes/Executive%20vote%20-%20June%2028%2C%202023.md' -q -O - 2>/dev/null)"
+    string public constant override description =
+        "some description";
+    address public a = 0x0000000000000000000000000000000000000000;
+}
 `;
-// TODO
 const badCode = `
-AUF
+pragma solidity 0.4.4;
+
+
+// if the executive document contains an address 0x0000000000000000000000000000000000000000
+contract C {
+    // Hash: cast keccak -- "$(wget 'https://raw.githubusercontent.com/makerdao/community/e4bf988dd35f82e2828e1ce02c6762ddd398ff92/governance/votes/Executive%20vote%20-%20June%2028%2C%202023.md' -q -O - 2>/dev/null)"
+    string public constant override description =
+        "some description";
+    address public a = 0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee;
+}
 `;
-// TODO
 exports.meta = {
     ruleId: 'addresses-match',
     type: 'miscellaneous',
-    // TODO
     docs: {
-        description: 'ASDF',
+        description: 'Addresses mentioned in the executive document have to be present in the source code.',
         category: 'Miscellaneous',
         examples: {
             good: [
                 {
-                    description: 'ASDF',
+                    description: 'The contract code contains the address that is present in the executive document.',
                     code: goodCode,
                 },
             ],
             bad: [
                 {
-                    description: 'ASDF',
+                    description: 'The contract code does not contain the address that is present in the executive document.',
                     code: badCode,
                 },
             ],
@@ -152,12 +166,14 @@ class ExecDocAddressesMatchSourceCode {
         const missingExecAddressesInSpell = compareAddresses(addresssesExecDoc, addressesSpell);
         const missingSpellAddressesInExec = compareAddresses(addressesSpell, addresssesExecDoc);
         if (missingExecAddressesInSpell.size !== 0) {
-            this.reporter.error({ type: 'SourceUnit', loc }, this.ruleId + '-source-code', 
+            this.reporter.error({ type: 'SourceUnit', loc }, `${this.ruleId}-source-code`, 
             // eslint-disable-next-line max-len
             `Expected addresses in the source code to match addresses in the executive document. Missing addresses:\n${Array.from(missingExecAddressesInSpell).join(',\n')}`);
         }
         if (missingSpellAddressesInExec.size !== 0) {
-            this.reporter.warn({ type: 'SourceUnit', loc }, this.ruleId + '-exec-doc', `Not all addresses in the source code are present in the executive document. Missing addresses:\n${Array.from(missingSpellAddressesInExec).join(',\n')}`);
+            this.reporter.warn({ type: 'SourceUnit', loc }, `${this.ruleId}-exec-doc`, 
+            // eslint-disable-next-line max-len
+            `Not all addresses in the source code are present in the executive document. Missing addresses:\n${Array.from(missingSpellAddressesInExec).join(',\n')}`);
         }
     }
 }
